77168a9f210e04f76a69e07de4786c84c8026967
Inkom007 <inkom0703@gmail.com>
2024-07-04 09:35:43 +0100

    add styles

--- src/Feedback/Feedback.jsx
+++ src/Feedback/Feedback.jsx
@@ -1,11 +1,15 @@
+import s from "./Feedback.module.css";
+
 const Feedback = ({ totalFeedback, good, neutral, bad }) => {
   return (
-    <ul>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-      <li>Total: {totalFeedback}</li>
-      <li>Positive: {Math.round((good / totalFeedback) * 100)}%</li>
+    <ul className={s.list}>
+      <li className={s.item}>Good: {good}</li>
+      <li className={s.item}>Neutral: {neutral}</li>
+      <li className={s.item}>Bad: {bad}</li>
+      <li className={s.item}>Total: {totalFeedback}</li>
+      <li className={s.item}>
+        Positive: {Math.round((good / totalFeedback) * 100)}%
+      </li>
     </ul>
   );
 };
--- src/Options/Options.jsx
+++ src/Options/Options.jsx
@@ -1,10 +1,22 @@
+import s from "./Options.module.css";
+
 const Options = ({ updateFeedback, totalFeedback, handleReset }) => {
   return (
     <div>
-      <button onClick={() => updateFeedback("good")}>Good</button>
-      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-      <button onClick={() => updateFeedback("bad")}>Bad</button>
-      {totalFeedback > 0 && <button onClick={handleReset}>Reset</button>}
+      <button className={s.button} onClick={() => updateFeedback("good")}>
+        Good
+      </button>
+      <button className={s.button} onClick={() => updateFeedback("neutral")}>
+        Neutral
+      </button>
+      <button className={s.button} onClick={() => updateFeedback("bad")}>
+        Bad
+      </button>
+      {totalFeedback > 0 && (
+        <button className={s.button} onClick={handleReset}>
+          Reset
+        </button>
+      )}
     </div>
   );
 };
